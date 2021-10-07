355063e422ffdf26e3c98b1416e65e7fb85a53ec
carinamarele <79100681+carinamarele@users.noreply.github.com>
2021-10-07 20:32:42 +0300

    Partea 1 Tema

---
+++ Lab1-IMR/Assets/Scripts/InteractionBetweenTargets.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionBetweenTargets : MonoBehaviour
+{
+    public GameObject obiect1;
+    public GameObject obiect2;
+    public GameObject cat;
+    public GameObject Fox;
+    Animator anim1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim1=Fox.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance=Vector3.Distance(obiect1.transform.position,obiect2.transform.position);
+
+        if(distance<1) { 
+               
+
+        }
+     Debug.Log(distance);
+    }
+}
