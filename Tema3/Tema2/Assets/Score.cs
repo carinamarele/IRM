7910d9bd709da72c1a64517d5e937121313c3f77
carinamarele <carina.marele@info.uaic.ro>
2021-10-22 14:55:02 +0300

    Tema3

---
+++ Tema3/Tema2/Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GameObject effectObject;
+
+    private void Start()
+    {
+        effectObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider otherCollider)
+    {
+        if (otherCollider.transform.gameObject.GetComponent<ThrowBallBehaviour>() != null)
+        {
+            Debug.Log("IRM");
+            effectObject.SetActive(true);
+        }
+    }
+}
---
+++ Tema3/Tema2/Assets/ThrowBallBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBallBehaviour : MonoBehaviour
+{
+
+    [SerializeField]
+    private float force = 3f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void Throw()
+    {
+        Debug.Log("hello");
+        Vector3 forceVector = transform.forward * force;
+        rb.AddForce(forceVector);
+    }
+}
